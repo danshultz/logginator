b39a056cf38578da775f18ffc21236c7ad9fce07
Dan Shultz <das0118@gmail.com>
2014-04-01 17:00:53 -0400

    include reset in log parser

--- lib/logginator/nginx_log_parser.rb
+++ lib/logginator/nginx_log_parser.rb
@@ -10,23 +10,25 @@ class Logginator::NginxLogParser
 
   attr_accessor :request_count, :total_request_time, :min, :max
 
+
   def initialize(scheme = nil)
     @scheme = scheme || "#{Socket.gethostname}"
+    reset
+  end
+
+
+  def reset
     @request_count = 0
     @total_request_time = 0.0
+    @min = nil
+    @max = nil
   end
+
 
   def parse_line(line)
     MATCHER.match(line, &method(:gather_stats))
   end
 
-  def gather_stats(data)
-    req_time = data[:request_time].to_f
-    self.request_count += 1
-    self.total_request_time += req_time
-    self.min = (min && min < req_time) ? min : req_time
-    self.max = (max && max > req_time) ? max : req_time
-  end
 
   def get_stats
     timestamp = Time.now.to_i
@@ -35,19 +37,29 @@ class Logginator::NginxLogParser
     }
   end
 
-  def stats_data
-    average = if (request_count == 0)
-      0
-    else
-      total_request_time.fdiv(request_count).round(5)
+  protected
+
+    def gather_stats(data)
+      req_time = data[:request_time].to_f
+      self.request_count += 1
+      self.total_request_time += req_time
+      self.min = (min && min < req_time) ? min : req_time
+      self.max = (max && max > req_time) ? max : req_time
     end
 
-    {
-      'count' => request_count,
-      'average' => average,
-      'min' => min || 0,
-      'max' => max || 0
-    }
-  end
+    def stats_data
+      average = if (request_count == 0)
+        0
+      else
+        total_request_time.fdiv(request_count).round(5)
+      end
+
+      {
+        'count' => request_count,
+        'average' => average,
+        'min' => min || 0,
+        'max' => max || 0
+      }
+    end
 
 end
