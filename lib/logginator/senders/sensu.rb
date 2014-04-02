fe6e8d63ceacc0d88675f23dda0452ad2b6ff8c4
Dan Shultz <das0118@gmail.com>
2014-04-02 15:07:28 -0400

    update sensu handler to take options and fix names

--- lib/logginator/senders/sensu.rb
+++ lib/logginator/senders/sensu.rb
@@ -2,15 +2,23 @@ require 'json'
 
 class Logginator::Senders::Sensu
 
-  def initialize(address, port=3030, sender=nil)
-    @sender ||= Logginator::Senders::UDP.new(address, port)
+  # Initializes a new sender which utilizes sensu to send data
+  #
+  # @param [String] address address which to send data
+  # @param [Integer] port port which to send data to
+  # @option opts [String] :sender optional sender internally
+  # @option opts [String] :name name of the check to send to sensu
+  # @option opts [Array] :handlers collection of handlers to use
+  def initialize(address, port=3030, opts={})
+    @sender = opts[:sender] || Logginator::Senders::UDP.new(address, port)
+    @opts = opts
   end
 
 
   def <<(data)
     payload = {
-      :handlers => ["metrics"],
-      :name => 'unicorn-metrics',
+      :handlers => @opts[:handlers] || ["metrics"],
+      :name => @opts[:name] || 'general-metrics',
       :status => 0,
       :output => data.to_s,
       :type => 'metric'
--- test/logginator/senders/sensu_test.rb
+++ test/logginator/senders/sensu_test.rb
@@ -13,8 +13,26 @@ class SendersSensuTest < MiniTest::Unit::TestCase
     sender << 'some data'
 
     expected = {
-      :handlers => ["metrics"],
-      :name => 'unicorn-metrics',
+      :handlers => ['metrics'],
+      :name => 'general-metrics',
+      :status => 0,
+      :output => 'some data',
+      :type => 'metric'
+    }.to_json
+
+    result = socket.recvfrom(100).shift
+    assert_equal(expected, result)
+  end
+
+
+  def test_it_takes_options
+    opts = { :name => 'check-name', :handlers => ['some-handler'] }
+    sender = Logginator::Senders::Sensu.new('127.0.0.1', port, opts)
+    sender << 'some data'
+
+    expected = {
+      :handlers => ['some-handler'],
+      :name => 'check-name',
       :status => 0,
       :output => 'some data',
       :type => 'metric'
